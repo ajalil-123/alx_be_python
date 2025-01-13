41d147b947f3ab5633f6ec36d9cb7d24e75ce6b7
ajalil-123 <a.zakaria@nithio.com>
2025-01-13 17:54:17 +0200

    create function to perform arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+
+def perform_operation(num1,num2,operation):
+    """performs aithmetic operations based on the input given"""
+    if operation == "add":
+        return num1 + num2         # adds the inputs and returns the results
+    elif operation == "subtract":
+        return num1 - num2          # subtracts the inputs and returns the results
+    elif operation == "multiply":
+        return num1 * num2           # multiplies the inputs and returns the results
+    elif operation == "divide":
+        if num2 != 0 :
+            return num1/num2
+
+
+
+
+
