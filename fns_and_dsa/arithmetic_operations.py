4447c44911d81684cc3e0224ea5117e088b5a93f
ajalil-123 <a.zakaria@nithio.com>
2025-01-13 18:08:32 +0200

    create function to perform arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,8 +8,8 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2           # multiplies the inputs and returns the results
     elif operation == "divide":
-        if num2 == 0 :
-            return "Division by zero error" 
+        if num2 == 0:
+            print("Cannot divide by zero.") # handles division by zero
         else:
             return num1/num2
 
