65265b2462e02aabf7e908b4f3590a0ac9fae550
ajalil-123 <a.zakaria@nithio.com>
2025-01-14 15:16:27 +0200

    created a shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -3,13 +3,13 @@ shopping_list = []
 
 #function to add items to the shopping list
 def add_item():
-    item = input("Enter the name of the item to add: ").strip()
+    item = input("Enter the item to add: ").strip()
     shopping_list.append(item)
     print(f"'{item}' has been added to the shopping list.")
 
 #function to remove items from the shopping list
 def remove_item():
-    item = input("Enter the name of the item to remove: ").strip()
+    item = input("Enter the item to remove: ").strip()
     if item in shopping_list:
         shopping_list.remove(item)
         print(f"'{item}' has been removed from the shopping list.")
