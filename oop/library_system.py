af62872f3034a5e37b761ad1eab6376314889e3d
Jalil <abduljalilzakaria1@gmail.com>
2025-01-28 14:51:59 +0200

    created a library system to mimic inheritance

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,6 +6,8 @@ class Book:
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
+#an ebook class that prints an ebook by title, author and file size 
+# This is a derived class that inherits some attributes from Book class
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title,author)
@@ -13,6 +15,8 @@ class EBook(Book):
     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
+# a print book class that inherits some attributes from Book class
+#the attributes it inherits are ptitle and author
 
 class PrintBook(Book):
     def __init__(self,title,author,page_count):
@@ -22,17 +26,19 @@ class PrintBook(Book):
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
-
+#create a composition class with methods such as add_book() and list_books()
 class Library:
     def __init__(self):
-        self.books = []
+        self.books = [] # create a list store of instances of Book,Ebook and Printbook
 
+    # method that adds a Book, EBook or PrintBook instance to the library
     def add_book(self, book):
         if isinstance(book, Book):  # Ensure only instances of Book or its subclasses are added
             self.books.append(book)
         else:
             print("Error: Only instances of Book, EBook, or PrintBook can be added to the library.")
-
+    
+    # a method that lists and prints the details of each book in the library
     def list_books(self):
         if not self.books:
             print("The library is currently empty.")
